c9da6c152153ea40e9dfa7ae4da4b978b9d79586
kalaiyarasan-lenovo <kalaivelliangiri2gmail.com>
2026-02-16 12:21:35 +0530

    adding footer on government_jobs

--- government_jobs.php
+++ government_jobs.php
@@ -257,6 +257,12 @@ $totalStateVacancies = $stateVacanciesRow['total_state_vacancies'] ?? 0;
             </table>
         </div>
     </div>
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container text-center">
+            <span class="text-muted">Copyright © 2026 [obcrights]</span><br>
+            <span class="text-muted">Powered by jobs.obcrights</span>
+        </div>
+    </footer>
 </body>
 
 </html>
